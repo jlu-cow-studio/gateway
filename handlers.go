81d94ca47ac44365967085bb5894aa9637526708
waynelee-lwc <1418551940@qq.com>
2023-03-21 16:23:33 +0800

    handlers

--- handlers.go
+++ handlers.go
@@ -6,4 +6,7 @@ func RegisterHandlers() {
 	server.GET("/ping", handler.Ping)
 
 	server.POST("/user/login", handler.UserLogin)
+	server.POST("/user/register", handler.UserRegister)
+	server.POST("/user/auth", handler.UserAuth)
+	server.GET("/user/info", handler.UserInfo)
 }
