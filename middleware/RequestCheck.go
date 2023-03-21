60d34dfa31e8475c10ee14bb2a774154e6f75f42
waynelee-lwc <1418551940@qq.com>
2023-03-21 16:09:05 +0800

    request check

---
+++ middleware/RequestCheck.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jlu-cow-studio/common/model/http_struct"
+)
+
+func RequestCheck() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		baseReq := &http_struct.OnlyBaseReq{}
+
+		buf := &bytes.Buffer{}
+		buf.ReadFrom(ctx.Request.Body)
+
+		fmt.Println("checking request: ", buf.String())
+
+		if err := json.Unmarshal(buf.Bytes(), baseReq); err != nil {
+			ctx.AbortWithStatusJSON(200, http_struct.InvalidRequest)
+			return
+		}
+
+		ctx.Set("base", baseReq.Base)
+	}
+}
