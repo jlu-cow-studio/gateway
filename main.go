7a2aa53030254ab5a178556cbff04c04a65d5ecc
waynelee-lwc <1418551940@qq.com>
2023-03-21 16:39:07 +0800

    handlers

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jlu-cow-studio/common/dal/redis"
 	"github.com/jlu-cow-studio/common/discovery"
+	"github.com/jlu-cow-studio/gateway/handler"
 	"github.com/jlu-cow-studio/gateway/middleware"
 )
 
@@ -23,4 +24,13 @@ func main() {
 	RegisterHandlers()
 
 	server.Run(ServerAddress)
+}
+
+func RegisterHandlers() {
+	server.GET("/ping", handler.Ping)
+
+	server.POST("/user/login", handler.UserLogin)
+	server.POST("/user/register", handler.UserRegister)
+	server.POST("/user/auth", handler.UserAuth)
+	server.GET("/user/info", handler.UserInfo)
 }
