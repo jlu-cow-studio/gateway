7bcd05ce8cfa44861398a58be838127df7d269cf
waynelee-lwc <1418551940@qq.com>
2023-03-23 19:09:06 +0800

    user register

--- handler/UserRegister.go
+++ handler/UserRegister.go
@@ -1,7 +1,77 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"encoding/json"
+	"log"
 
-func UserRegister(ctx *gin.Context) {
+	"github.com/gin-gonic/gin"
+	"github.com/jlu-cow-studio/common/dal/rpc"
+	"github.com/jlu-cow-studio/common/dal/rpc/base"
+	"github.com/jlu-cow-studio/common/dal/rpc/user_core"
+	"github.com/jlu-cow-studio/common/model/http_struct"
+	"github.com/jlu-cow-studio/common/model/http_struct/user"
+	"github.com/sanity-io/litter"
+)
 
+func UserRegister(c *gin.Context) {
+	regReq := new(user.UserRegisterReq)
+	regRes := &user.UserRegisterRes{
+		Base: http_struct.ResBase{
+			Code:    "400",
+			Message: "failed",
+		},
+	}
+	defer c.JSON(200, regRes)
+	defer log.Println("response: ", litter.Sdump(regRes))
+
+	bodyb, ok := c.Get("body")
+	if !ok {
+		regRes.Base.Message = "error when get body"
+		return
+	}
+
+	log.Println("request body: ", string(bodyb.([]byte)))
+	if err := json.Unmarshal(bodyb.([]byte), regReq); err != nil {
+		regRes.Base.Message = err.Error()
+		return
+	}
+
+	log.Println("request: ", litter.Sdump(regReq))
+
+	conn, err := rpc.GetConn(UserCoreServiceName)
+	if err != nil {
+		log.Printf("get rpc conn error: %s\n", err.Error())
+		regRes.Base.Message = err.Error()
+		return
+	}
+
+	cli := user_core.NewUserCoreServiceClient(conn)
+
+	rpcUserRegisterReq := &user_core.UserRegisterReq{
+		Base: &base.BaseReq{
+			Token: regReq.Base.Token,
+			Logid: regReq.Base.LogId,
+		},
+		UserInfo: &user_core.UserInfo{
+			Username: regReq.Username,
+			Password: regReq.Password,
+			Province: regReq.Province,
+			City:     regReq.City,
+			District: regReq.District,
+		},
+	}
+
+	log.Printf("rpc request: %+v\n", rpcUserRegisterReq)
+
+	rpcUserRegisterRes, err := cli.UserRegister(context.Background(), rpcUserRegisterReq)
+	if err != nil {
+		log.Printf("rpc call error: %s\n", err.Error())
+		regRes.Base.Message = err.Error()
+		return
+	}
+
+	regRes.Base.Code = rpcUserRegisterRes.Base.Code
+	regRes.Base.Message = rpcUserRegisterRes.Base.Message
+	log.Printf("rpc response: %+v\n", rpcUserRegisterRes)
 }
