d73b26c59564bcdd35fe80270df237a2adcc6be4
waynelee-lwc <1418551940@qq.com>
2023-03-23 18:13:13 +0800

    bug fix

--- handler/UserLogin.go
+++ handler/UserLogin.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jlu-cow-studio/common/dal/rpc"
@@ -11,12 +12,12 @@ import (
 	"github.com/jlu-cow-studio/common/dal/rpc/user_core"
 	"github.com/jlu-cow-studio/common/model/http_struct"
 	"github.com/jlu-cow-studio/common/model/http_struct/user"
+	"github.com/sanity-io/litter"
 )
 
 const UserCoreServiceName = "cowstudio/user-core"
 
 func UserLogin(c *gin.Context) {
-
 	loginReq := new(user.UserLoginReq)
 	loginRes := &user.UserLoginRes{
 		Base: http_struct.ResBase{
@@ -25,6 +26,7 @@ func UserLogin(c *gin.Context) {
 		},
 	}
 	defer c.JSON(200, loginRes)
+	defer log.Println("response: ", litter.Sdump(loginRes))
 
 	buf := &bytes.Buffer{}
 	if _, err := buf.ReadFrom(c.Request.Body); err != nil {
@@ -39,6 +41,7 @@ func UserLogin(c *gin.Context) {
 
 	conn, err := rpc.GetConn(UserCoreServiceName)
 	if err != nil {
+		log.Printf("get rpc conn error: %s\n", err.Error())
 		loginRes.Base.Message = err.Error()
 		return
 	}
@@ -54,9 +57,17 @@ func UserLogin(c *gin.Context) {
 		Password: loginReq.Password,
 	}
 
-	rpcUserLoginRes, _ := cli.UserLogin(context.Background(), rpcUserLoginReq)
+	log.Printf("rpc request: %+v\n", rpcUserLoginReq)
+
+	rpcUserLoginRes, err := cli.UserLogin(context.Background(), rpcUserLoginReq)
+	if err != nil {
+		log.Printf("rpc call error: %s\n", err.Error())
+		loginRes.Base.Message = err.Error()
+		return
+	}
 
 	loginRes.Base.Code = rpcUserLoginRes.Base.Code
 	loginRes.Base.Message = rpcUserLoginRes.Base.Message
 	loginRes.Token = rpcUserLoginRes.Token
+	log.Printf("rpc response: %+v\n", rpcUserLoginRes)
 }
