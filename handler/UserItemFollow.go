44365db4bd810afeced3677d27183f41bf954b5b
alone-yue <zhangyue.alone@bytedance.com>
2023-04-15 12:23:12 +0800

    feat:add UserItem func

---
+++ handler/UserItemFollow.go
@@ -0,0 +1,15 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func AddUserItemFollow(c *gin.Context) {
+	panic("")
+}
+
+func DelUserItemFollow(c *gin.Context) {
+	panic("")
+}
+
+func UserItemFollowList(c *gin.Context) {
+	panic("")
+} 
